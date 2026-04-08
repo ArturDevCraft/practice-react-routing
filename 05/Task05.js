83bda24913a447eef19f6aca884ff603d169d5d5
ArturDevCraft <artur.czerwiak@gmail.com>
2026-04-08 20:53:15 +0200

    Task 05 completed

--- 05/Task05.js
+++ 05/Task05.js
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Route, Switch, useHistory } from 'react-router-dom';
+import Shop from '../src/components/Shop';
+import products from './../src/products.json';
 
 const Task05 = () => {
-    return (
-        <h1>Task05</h1>
-    );
-}
+	const [minPrice, setMinPrice] = useState('');
+	const [maxPrice, setMaxPrice] = useState('');
+	const [search, setSearch] = useState('');
+	const history = useHistory();
+	useEffect(() => {
+		const newMinPrice = minPrice == '' ? ' ' : minPrice;
+		const newMaxPrice = maxPrice == '' ? ' ' : maxPrice;
+		const newSearch = search == '' ? ' ' : search;
+		const link = `/task05/[${newMinPrice}],[${newMaxPrice}]-[${newSearch}]`;
+		history.push(link);
+	}, [minPrice, maxPrice, search]);
+
+	return (
+		<>
+			<h1>Task05</h1>
+			<label>
+				Min price:
+				<input
+					value={minPrice}
+					type="number"
+					onChange={(e) => setMinPrice(e.target.value)}
+				/>
+			</label>
+
+			<br />
+			<label>
+				Max price:
+				<input
+					value={maxPrice}
+					type="number"
+					onChange={(e) => setMaxPrice(e.target.value)}
+				/>
+			</label>
+			<br />
+			<label>
+				Search:
+				<input value={search} onChange={(e) => setSearch(e.target.value)} />
+			</label>
+			<Switch>
+				<Route path="/task05/[:minPrice],[:maxPrice]-[:search]">
+					<Shop products={products} />
+				</Route>
+				<Route path="/task05">
+					<Shop products={products} />
+				</Route>
+			</Switch>
+		</>
+	);
+};
 
 export default Task05;
-
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -3,25 +3,44 @@ import Product from './Product';
 import { useParams, Link } from 'react-router-dom';
 
 const Shop = ({ products: propsProducts }) => {
-	const { cat = null, sort = null } = useParams();
+	const {
+		cat = null,
+		sort = null,
+		minPrice = null,
+		maxPrice = null,
+		search = null,
+	} = useParams();
 
 	if (sort === 'price-asc') {
 		propsProducts.sort((a, b) => a.price - b.price);
 	}
 
 	if (sort === 'price-desc') {
-        propsProducts.sort((a, b) => b.price - a.price);
+		propsProducts.sort((a, b) => b.price - a.price);
 	}
 
-	const products = propsProducts.filter(
+	let products = propsProducts.filter(
 		(prod) =>
 			cat?.toLowerCase() === prod.category.toLowerCase() || cat === null,
 	);
+	console.log(search);
+
+	if (minPrice != null && maxPrice != null && search != null) {
+		console.log('ok');
+		products = propsProducts.filter(
+			(prod) =>
+				(minPrice == ' ' ? true : prod.price >= minPrice) &&
+				(maxPrice == ' ' ? true : prod.price <= maxPrice) &&
+				(search == ' '
+					? true
+					: prod.name.toLowerCase().includes(search.toLowerCase())),
+		);
+	}
 
 	return (
 		<section>
 			{products.map((p) => (
-				<Link to={`/task02/product-${p.id}`}>
+				<Link key={p.id} to={`/task02/product-${p.id}`}>
 					<Product {...p} />
 				</Link>
 			))}
