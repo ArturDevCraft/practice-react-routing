2af13de6ff9e41cdd752db6a51661af67160eb08
ArturDevCraft <artur.czerwiak@gmail.com>
2026-04-08 19:18:34 +0200

    Task 03 completed

--- 03/Task03.js
+++ 03/Task03.js
@@ -2,12 +2,28 @@ import React from 'react';
 
 import products from './../src/products.json';
 import Shop from '../src/components/Shop';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 
 const Task03 = () => {
+	const categories = [...new Set(products.map((prod) => prod.category))];
+	const nav = (
+		<nav>
+			<ul>
+				<li>
+					<Link to={`/task03`}>All categories</Link>
+				</li>
+				{categories.map((cat) => (
+					<li>
+						<Link to={`/task03/${cat}`}>{cat}</Link>
+					</li>
+				))}
+			</ul>
+		</nav>
+	);
 	return (
 		<>
 			<h1>Task03</h1>
+			{nav}
 			<Switch>
 				<Route path="/task03/:cat">
 					<Shop products={products} />
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Product from './Product';
+import { useParams, Link } from 'react-router-dom';
 
-const Shop = ({products}) => {
-    return (
-        <section>
-            { products.map(p => <Product {...p} />)}
-        </section>
-    );
-}
+const Shop = ({ products: propsProducts }) => {
+	const { cat = null } = useParams();
+
+	const products = propsProducts.filter(
+		(prod) =>
+			cat?.toLowerCase() === prod.category.toLowerCase() || cat === null,
+	);
+
+	return (
+		<section>
+			{products.map((p) => (
+				<Link to={`/task02/product-${p.id}`}>
+					<Product {...p} />
+				</Link>
+			))}
+		</section>
+	);
+};
 
 export default Shop;
