49cfa23253a52c9dbb3b9d81b33fa4d757c1e79d
ArturDevCraft <artur.czerwiak@gmail.com>
2026-04-08 19:40:19 +0200

    Task 04 completed

--- 04/Task04.js
+++ 04/Task04.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Route, Switch, Link, useHistory } from 'react-router-dom';
+import Shop from '../src/components/Shop';
+import products from './../src/products.json';
 
 const Task04 = () => {
-    return (
-        <h1>Task04</h1>
-    );
-}
+	const history = useHistory();
+	const changeHandler = (e) => {
+		const sort = e.target.value;
+		const link = `/task04/${sort}`;
+		history.push(link);
+	};
+	return (
+		<>
+			<h1>Task04</h1>
+			<select onChange={changeHandler}>
+				<option>-- Select sort --</option>
+				<option value="price-asc">Sort by price asc</option>
+				<option value="price-desc">Sort by price desc</option>
+			</select>
+			<Route path="/task04/:sort">
+				<Shop products={products} />
+			</Route>
+		</>
+	);
+};
 
 export default Task04;
-
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -3,7 +3,15 @@ import Product from './Product';
 import { useParams, Link } from 'react-router-dom';
 
 const Shop = ({ products: propsProducts }) => {
-	const { cat = null } = useParams();
+	const { cat = null, sort = null } = useParams();
+
+	if (sort === 'price-asc') {
+		propsProducts.sort((a, b) => a.price - b.price);
+	}
+
+	if (sort === 'price-desc') {
+        propsProducts.sort((a, b) => b.price - a.price);
+	}
 
 	const products = propsProducts.filter(
 		(prod) =>
